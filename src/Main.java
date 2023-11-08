9439d659bf49f24b0f90a2c9e123482240bb9f57
Мария <kolesnikovamaria169@gmail,com>
2023-11-08 22:10:58 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int cost_of_mili = 20;
+        int price_ticket = 13550;
+        double number_mili;
+
+        number_mili=Math.floor(price_ticket/cost_of_mili);
+        System.out.println(number_mili);
+
+            }
+}
