1728185b861f33899fb4978fca9a1094d2040e50
Мария <kolesnikovamaria169@gmail,com>
2023-11-11 16:12:44 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ public class Main {
     public static void main(String[] args) {
         int costOfMili = 20;
         int priceTicket = 13550;
-        double numberMili;
-        numberMili = Math.floor(priceTicket / costOfMili);
+        double numberMili = priceTicket / costOfMili;
         System.out.println(numberMili);
 
     }
